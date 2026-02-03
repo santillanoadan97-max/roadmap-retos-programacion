0d180631adc5cbab1fe3acd993e0dfb7323b42b8
santillanoadan97-max <santillanoadan97@gmail.com>
2026-02-03 09:07:32 -0600

    Create adan.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/adan.py
@@ -0,0 +1,33 @@
+# https://www.python.org/
+
+# comentario en una linea
+
+"""
+esto tambien es
+un comentario en 
+varias lineas
+"""
+
+'''
+esto tambien es 
+un comentario en
+varias lineas
+'''
+
+My_variable = "Mi variable"
+My_variable = "nuevo valor de mi variable"
+
+"en phyton todas son variables no hay constantes"
+
+MY_CONSTANT = "mi constante" # por convencion
+MY_CONSTANT = "hbsjbcjb"
+
+# tipos de datos
+My_integer = 10
+My_float = 1.5
+My_boolean = False
+My_boolean = True
+MY_string = "mi cadena de texto"
+My_other_string = 'mi otra cadena de texto'
+
+Print('¡hola, phyton!')
