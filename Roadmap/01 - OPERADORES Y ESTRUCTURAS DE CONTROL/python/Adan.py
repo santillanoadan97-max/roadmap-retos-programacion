0d08ea3cae87e40597442977b5ff94f82e4889c4
santillanoadan97-max <santillanoadan97@gmail.com>
2026-02-03 11:32:28 -0600

    Update Adan.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Adan.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Adan.py
@@ -1,7 +1,23 @@
 # operadores aritmeticos
-Print(Suma 2+4)
-Print(Resta 10-6)
-Print(Multiplicacion 2*3)
-Print(Division 3/9)
-Print(Modulo 7%3)
-Print(Potenciacion 2 ** 3)
+      Print(Suma 2+4)
+      Print(Resta 10-6)
+      Print(Multiplicacion 2*3)
+      Print(Division 3/9)
+      Print(Modulo 7%3) # es lo que sobra de la division
+      Print(Potenciacion 2 ** 3)
+
+# operadores de comparacion
+      Print(Igual 5 == 5) True # verifica si son iguales 
+      Print(Distinto 3 != 7) True #verifica si son distintos
+      Print(Mayor_que 5 > 10) False # verifica si el numero de la izquierda es mayor que el de la derecha
+      Print(Menor_que 9 < 7) False # verifiva si el numero de la izquierda es menor que el de la derecha
+      Print(Mayor_o_igual_que 7 >= 7) True # verifica si el numero de la izquierda es mayor o igual que el de la derecha
+      Print(Menor_o_igual_que 4 <= 4) True # verifica si el numero de la izquierda es menor o igual que el de la derecha
+
+# operadores a nivel bits
+      Print(OperadorAND A = 5 "0101 en binario" B = 3 "0011 en binario" A & B = 1 "0001 en binario")
+      Print
+      Print
+      Print
+      Print
+      Print
